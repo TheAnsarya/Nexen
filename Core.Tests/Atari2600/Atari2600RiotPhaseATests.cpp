0da4baee16e4929a797c6e3d3fe7999bc60f70b7
Andy Hubbard <me@ansarya.com>
2026-03-20 13:39:08 -0400

    test: add RIOT divider-transition edge matrix tests - #777

--- Core.Tests/Atari2600/Atari2600RiotPhaseATests.cpp
+++ Core.Tests/Atari2600/Atari2600RiotPhaseATests.cpp
@@ -134,4 +134,101 @@ namespace {
 		EXPECT_EQ(runA[4], 0u);
 		EXPECT_EQ(runA[5], 0u);
 	}
+
+	TEST(Atari2600RiotPhaseATests, TimerDividerTransitionEdgeMatrixMaintainsExpectedUnderflowEdges) {
+		struct RiotEdgeCase {
+			const char* Name;
+			uint16_t InitialDividerReg;
+			uint8_t InitialTimerValue;
+			uint32_t PreCycles;
+			uint16_t TransitionDividerReg;
+			uint8_t TransitionTimerValue;
+			uint32_t PostCycles;
+			bool ExpectUnderflow;
+			uint32_t ExpectInterruptEdges;
+		};
+
+		const std::array<RiotEdgeCase, 4> edgeCases = {{
+			{"fast_to_slow_no_underflow_yet", 0x0284, 0x01, 1, 0x0285, 0x01, 8, false, 0},
+			{"fast_to_slow_underflow", 0x0284, 0x01, 1, 0x0285, 0x01, 16, true, 1},
+			{"slow_to_fast_underflow", 0x0285, 0x01, 7, 0x0284, 0x01, 2, true, 1},
+			{"long_divider_underflow", 0x0286, 0x00, 64, 0x0286, 0x00, 0, true, 1},
+		}};
+
+		for (const RiotEdgeCase& edgeCase : edgeCases) {
+			SCOPED_TRACE(string("case=") + edgeCase.Name
+				+ ";pre=" + std::to_string(edgeCase.PreCycles)
+				+ ";post=" + std::to_string(edgeCase.PostCycles));
+
+			Emulator emu;
+			Atari2600Console console(&emu);
+			console.Reset();
+
+			console.DebugWriteCartridge(edgeCase.InitialDividerReg, edgeCase.InitialTimerValue);
+			if (edgeCase.PreCycles > 0) {
+				console.StepCpuCycles(edgeCase.PreCycles);
+			}
+
+			if (edgeCase.PostCycles > 0 || edgeCase.TransitionDividerReg != edgeCase.InitialDividerReg || edgeCase.TransitionTimerValue != edgeCase.InitialTimerValue) {
+				console.DebugWriteCartridge(edgeCase.TransitionDividerReg, edgeCase.TransitionTimerValue);
+			}
+
+			if (edgeCase.PostCycles > 0) {
+				console.StepCpuCycles(edgeCase.PostCycles);
+			}
+
+			Atari2600RiotState state = console.GetRiotState();
+			EXPECT_EQ(state.TimerUnderflow, edgeCase.ExpectUnderflow)
+				<< "case=" << edgeCase.Name << ";timer=" << state.Timer;
+			EXPECT_EQ(state.InterruptFlag, edgeCase.ExpectUnderflow)
+				<< "case=" << edgeCase.Name << ";interrupt=" << state.InterruptFlag;
+			EXPECT_EQ(state.InterruptEdgeCount, edgeCase.ExpectInterruptEdges)
+				<< "case=" << edgeCase.Name << ";edges=" << state.InterruptEdgeCount;
+		}
+	}
+
+	TEST(Atari2600RiotPhaseATests, TimerDividerTransitionDigestIsDeterministicAcrossRuns) {
+		auto mixDigest = [](uint64_t hash, uint64_t value) {
+			hash ^= value + 0x9e3779b97f4a7c15ULL + (hash << 6) + (hash >> 2);
+			return hash;
+		};
+
+		auto runDigest = [&]() {
+			uint64_t digest = 0xcbf29ce484222325ULL;
+			const std::array<uint16_t, 4> dividerRegs = {0x0284, 0x0285, 0x0286, 0x0287};
+
+			for (uint16_t dividerReg : dividerRegs) {
+				for (uint8_t timerSeed = 0; timerSeed < 3; timerSeed++) {
+					Emulator emu;
+					Atari2600Console console(&emu);
+					console.Reset();
+
+					console.DebugWriteCartridge(dividerReg, timerSeed);
+					console.StepCpuCycles(130);
+
+					uint8_t instatA = console.DebugReadCartridge(0x0286);
+					uint8_t instatB = console.DebugReadCartridge(0x0286);
+					uint8_t instatMirror = console.DebugReadCartridge(0x0287);
+					uint8_t intim = console.DebugReadCartridge(0x0284);
+					Atari2600RiotState state = console.GetRiotState();
+
+					digest = mixDigest(digest, dividerReg);
+					digest = mixDigest(digest, timerSeed);
+					digest = mixDigest(digest, instatA);
+					digest = mixDigest(digest, instatB);
+					digest = mixDigest(digest, instatMirror);
+					digest = mixDigest(digest, intim);
+					digest = mixDigest(digest, state.InterruptEdgeCount);
+					digest = mixDigest(digest, state.Timer);
+				}
+			}
+
+			return digest;
+		};
+
+		uint64_t runA = runDigest();
+		uint64_t runB = runDigest();
+		EXPECT_EQ(runA, runB);
+		EXPECT_NE(runA, 0u);
+	}
 }
