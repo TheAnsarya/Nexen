28d9b7b4ef1d0d926fab80e2ed27ca88dc8f97a4
Andy Hubbard <me@ansarya.com>
2026-05-10 14:02:03 -0400

    fix: recover movieconverter build and continue cleanup batch - #2178

--- MovieConverter/Enums.cs
+++ MovieConverter/Enums.cs
@@ -1,4 +1,4 @@
-namespace Nexen.MovieConverter;
+﻿namespace Nexen.MovieConverter;
 
 /// <summary>
 /// Represents the format of a TAS movie file
@@ -15,7 +15,7 @@ public enum MovieFormat {
 
 	/// <summary>Obsolete alias for LegacyMmoMmm.</summary>
 	[System.Obsolete("Use LegacyMmoMmm instead.")]
-	Nexen = LegacyMmoMmm,
+	NexenLegacy = LegacyMmoMmm,
 
 	/// <summary>Snes9x format (.smv)</summary>
 	Smv,
